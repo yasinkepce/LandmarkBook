94351f2f7a7a31e52797ae80c7c31b517372d907
Yasin Kepce <yasinkepce@hotmail.com>
2020-04-20 21:00:20 +0400

    Segue Öncesi

--- LandmarkBook/ViewController.swift
+++ LandmarkBook/ViewController.swift
@@ -12,12 +12,41 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
 
     @IBOutlet weak var myTableView: UITableView!
     
+    var landmarkNames = [String]()
+    var landmarkImages = [UIImage]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
         myTableView.delegate = self
         myTableView.dataSource = self
+        
+        
+        
+        
+        landmarkNames.append("TOKAT SAAT KULESi")
+        landmarkNames.append("LONDON EYE")
+        landmarkNames.append("STATUE OF LIBERTY")
+        landmarkNames.append("KREMLIN")
+        landmarkNames.append("EYFEL KULESI")
+        
+        
+        landmarkImages.append(UIImage(named: "saatkulesi")!)
+        landmarkImages.append(UIImage(named: "londoneye")!)
+        landmarkImages.append(UIImage(named: "liberty")!)
+        landmarkImages.append(UIImage(named: "kremlin")!)
+        landmarkImages.append(UIImage(named: "eyfel")!)
+    }
+    
+   
+    //Sola çekerek silme özelliği ekliyoruz
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        
+        landmarkNames.remove(at: indexPath.row)
+        landmarkImages.remove(at: indexPath.row)
+        myTableView.deleteRows(at: [indexPath], with: UITableView.RowAnimation.fade)
+        
     }
     
     
@@ -25,13 +54,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "deneme"
+        cell.textLabel?.text = landmarkNames[indexPath.row]
         return cell
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return 10 // satır sayısını belirliyoruz.
+        return landmarkNames.count // satır sayısını belirliyoruz.
     }
 
 
