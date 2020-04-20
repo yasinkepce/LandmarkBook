89ec67dad295d255d8ccede618ff9449efac9589
Yasin Kepce <yasinkepce@hotmail.com>
2020-04-20 19:38:24 +0400

    Initial Commit

---
+++ LandmarkBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LandmarkBook
+//
+//  Created by Yasin Kepce on 20.04.2020.
+//  Copyright © 2020 Yasin Kepce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
