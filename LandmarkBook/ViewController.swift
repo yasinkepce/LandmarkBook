da69f6df24472ea6172ea8cf891d3b790d3af73c
Yasin Kepce <yasinkepce@hotmail.com>
2020-04-20 20:09:42 +0400

    ilk tanımlamaları yapıp view controller ekledik, sadece deneme yazıyor 10 satır

--- LandmarkBook/ViewController.swift
+++ LandmarkBook/ViewController.swift
@@ -8,11 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource { //bunlar ekleniyor? Şimdilik açıkmalasını bilmiyorum
 
+    @IBOutlet weak var myTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        myTableView.delegate = self
+        myTableView.dataSource = self
+    }
+    
+    
+    //Bu iki fonksiyonu mutlaka yazmamız gerekiyor
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "deneme"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return 10 // satır sayısını belirliyoruz.
     }
 
 
